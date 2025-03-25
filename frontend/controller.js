e7ef6f5e85a7a6c37cdc272227b80ccea6b98bcd
Srestha Mukherjee <99588466+SresthaMukherjee@users.noreply.github.com>
2025-03-25 11:17:25 +0530

    Delete frontend/controller.js

--- frontend/controller.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    //Display Speak Message
-    ele.expose(DisplayMessage);
-    function DisplayMessage(message){
-    $(".siri-message li:first").text(message);
-    $(".siri-message").textillate("start");
-    }
-});
