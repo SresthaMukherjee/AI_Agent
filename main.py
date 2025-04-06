52a29c1fee0aeae0034d5c05ccfc3dec71657e62
SresthaMukherjee <srestha.2002@gmail.com>
2025-04-06 21:39:43 +0530

    cross platform and req txt

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 import eel
+import platform
 from backend.feature import *
 from backend.command import *
 
 def start():
     eel.init("frontend")
-    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    url = "http://localhost:8000/index.html"
+    system_os = platform.system()
+
+    if system_os == "Windows":
+        os.system(f'start msedge.exe --app="{url}"')  # Or use 'start chrome' if Chrome is default
+    elif system_os == "Darwin":  # macOS
+        os.system(f'open "{url}"')
+    else:  # Linux or others
+        os.system(f'xdg-open "{url}"')  # Works in most Linux distros
 
     playAssistantSound()
 
-    eel.start("index.html",mode=None,host="localhost",block=True)
-
-
-
-
+    eel.start("index.html", mode=None, host="localhost", block=True)
